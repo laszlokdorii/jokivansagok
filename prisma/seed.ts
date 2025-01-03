d091a8e3a0b3facda1c92c8b9e57df2e48e461b2
laszlokdorii <laszlok.dora@gmail.com>
2025-01-03 20:09:05 +0100

    prisma, seed, adatbazis

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { faker, LoremModule } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
+ 
+async function main() {
+    
+    for(let i=0; i<15; i++){
+
+        await prisma.kivansagok.create({
+            data:{
+                kivanonev: faker.person.fullName().toString(),
+                tartalom: faker.lorem.paragraph().toString(),
+                kivansagido: new Date(),
+            }
+        }),
+
+        
+    };
+}
+ 
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)})
